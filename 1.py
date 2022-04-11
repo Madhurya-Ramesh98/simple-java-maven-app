2a5ef2c76d8af0fe6a0b926a0646bc237bba996b
Madhurya-Ramesh <94900616+Madhurya-Ramesh@users.noreply.github.com>
2022-04-11 14:56:49 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,8 +5,8 @@ import json
 # EMAIL_CHANDRA = os.environ['EMAIL_CHANDRA']
 # API_CHANDRA = os.environ['API_CHANDRA']
 
-EMAIL_CHANDRA = {{ secrets.EMAIL_CHANDRA }}
-API_CHANDRA = {{ secrets.API_CHANDRA }}
+EMAIL_CHANDRA = '$ {{ secrets.EMAIL_CHANDRA }}'
+API_CHANDRA = '$ {{ secrets.API_CHANDRA }}'
 
 url = "https://chandratech.atlassian.net/rest/api/3/issue/Ap-17"
 # token = 'EqijvLWIwETIVbQXEgBFEA26'
