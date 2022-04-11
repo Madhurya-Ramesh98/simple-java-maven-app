5617b1ada764fc691471d49af07349a9210e44af
Madhurya-Ramesh <94900616+Madhurya-Ramesh@users.noreply.github.com>
2022-04-11 12:22:57 +0530

    Update vijeth.py

--- vijeth.py
+++ vijeth.py
@@ -6,7 +6,7 @@ issueType=[]
 
 response = requests.get('https://vijeths.atlassian.net/rest/api/3/issue/createmeta?projectKeys=PP&expand=projects.issuetypes.fields',auth=('vijeth565@gmail.com','AcylDRWtlKDjb4CoOL4W310D'))
 
-print("---------------------------------")
+
 
 
 
