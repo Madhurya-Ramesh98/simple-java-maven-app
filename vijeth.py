e545aa64a2863ae610e6cfd481d07762d62c9fad
Madhurya-Ramesh <94900616+Madhurya-Ramesh@users.noreply.github.com>
2022-04-11 12:21:51 +0530

    Update vijeth.py

--- vijeth.py
+++ vijeth.py
@@ -4,7 +4,7 @@ import json
 
 issueType=[]
 
-response = requests.get('https://vijeths.atlassian.net/rest/api/3/issue/createmeta?projectKeys=PP&expand=projects.issuetypes.fields',auth=('vijeth565@gmail.com','6pmJmZTxKpLjePpnUx8j7DF3'))
+response = requests.get('https://vijeths.atlassian.net/rest/api/3/issue/createmeta?projectKeys=PP&expand=projects.issuetypes.fields',auth=('vijeth565@gmail.com','AcylDRWtlKDjb4CoOL4W310D'))
 
 print("---------------------------------")
 
