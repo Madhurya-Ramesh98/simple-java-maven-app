d517708c93ecc64e34ca27c35bebc20c0c0d36f8
Madhurya-Ramesh <94900616+Madhurya-Ramesh@users.noreply.github.com>
2022-04-11 13:10:18 +0530

    Create vijeth2.py

---
+++ vijeth2.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import os
+
+EMAIL = os.environ['EMAIL']
+JIRA_API = os.environ['JIRA_API']
+
+
+url = "https://vijeths.atlassian.net/rest/api/3/search?jql=project = "SM""
+
+headers = {
+   "Accept": "application/json",
+   "Content-Type": "application/json"
+}
+
+r = requests.get(url, headers=headers, auth=(EMAIL, JIRA_API))
+
+#print(r.text)
+#print(type(r))
+
+
+print(json.dumps(json.loads(r.text), sort_keys=True, indent=4, separators=(",", ": ")))
