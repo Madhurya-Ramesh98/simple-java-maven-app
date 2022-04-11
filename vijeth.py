9b8279641641d43a149e04ac93edb1e82257f71a
Madhurya-Ramesh <94900616+Madhurya-Ramesh@users.noreply.github.com>
2022-04-11 12:05:14 +0530

    Create vijeth.py

---
+++ vijeth.py
@@ -0,0 +1,19 @@
+import requests
+
+import json
+
+issueType=[]
+
+response = requests.get('https://vijeths.atlassian.net/rest/api/3/issue/createmeta?projectKeys=PP&expand=projects.issuetypes.fields',auth=('vijeth565@gmail.com','6pmJmZTxKpLjePpnUx8j7DF3'))
+
+
+
+
+
+for data in response.json()['projects']:
+
+    for d in (data['issuetypes']):
+
+        issueType.append(d)
+
+print(d)
