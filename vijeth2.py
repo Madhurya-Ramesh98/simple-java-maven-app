c13ff8790b33d4d32d85f0a9e482227100f3d2b0
Madhurya-Ramesh <94900616+Madhurya-Ramesh@users.noreply.github.com>
2022-04-11 13:16:23 +0530

    Update vijeth2.py

--- vijeth2.py
+++ vijeth2.py
@@ -6,7 +6,7 @@ EMAIL = os.environ['EMAIL']
 JIRA_API = os.environ['JIRA_API']
 
 
-url = "https://vijeths.atlassian.net/rest/api/3/search?jql=project = "SM""
+url = 'https://vijeths.atlassian.net/rest/api/3/search?jql=project = "SM"'
 
 headers = {
    "Accept": "application/json",
